8443e8a73f28a93de338d351dbbcbf5baeb3f343
Erick Vicente Agrazal Lopez <erick.agrazal@gmail.com>
2021-07-29 16:03:44 -0500

    Added tests documentation in the README.md

--- answer.1/solution/README.md
+++ answer.1/solution/README.md
@@ -20,5 +20,11 @@ npm run build
 npm run lint
 ```
 
+### Tests
+```
+npm run test:unit
+```
+
+
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- answer.1/solution/src/App.vue
+++ answer.1/solution/src/App.vue
@@ -5,6 +5,14 @@
 <script>
   import FirstSolution from './components/FirstSolution.vue'
 
+  /*
+   range. 
+    Description: A function that given a size and an optional startAt
+                 generates an array.
+    @param size: size of the array.
+    @param startAt: at which number should the array start.
+    @returns array: an array of generated numbers.
+   */
   const range = (size, startAt = 0) => { return [...Array(size).keys()].map(i => i + startAt) };
 
   export default {
